1692e3a3ddf83b2812ea06dc184d7eb6c5f58601
Ralf Bensmann <ralf@art-of-coding.eu>
2015-06-06 18:13:04 +0200

    Added MyFirstIntelliJPlugin

---
+++ MyFirstIntelliJPlugin/src/eu/artofcoding/intellij/plugin/RalfAction.java
@@ -0,0 +1,15 @@
+package eu.artofcoding.intellij.plugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * User: rbe
+ * Date: 17.05.15
+ * Time: 11:19
+ */
+public class RalfAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
