a31ae1ff20c1cf5fed5385fe20136222ca22c773
Ralf Bensmann <ralf@art-of-coding.eu>
2015-06-08 22:45:18 +0200

    Added vert.x example

---
+++ eu.artofcoding.vertx.project/src/main/resources/ping_verticle.py
@@ -0,0 +1,9 @@
+from core.event_bus import EventBus
+
+
+def handler(msg):
+    msg.reply('pong!')
+    print 'sent back pong Python!'
+
+
+EventBus.register_handler('ping-address', handler=handler)
